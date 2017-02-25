4889e7fe2324386723f8065edd5bd7c63ce622d9
mayeut <mayeut@users.noreply.github.com>
2017-02-25 18:17:43 +0100

    Single version source

--- pybase64/__init__.py
+++ pybase64/__init__.py
@@ -1,13 +1,11 @@
+from ._version import __version__
+
 try:
     import _pybase64
     _has_extension = True
 except ImportError:
     import base64
     _has_extension = False
-
-"""
-import base64
-"""
 
 def standard_b64encode(binarydata):
     if _has_extension:
--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[:2] < (3, 4):
 
 here = path.abspath(path.dirname(__file__))
 
+# Get version
+exec(open(path.join(here, 'pybase64/_version.py')).read())
+
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
@@ -146,7 +149,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version=__version__,
 
     description='Fast Base64 encoding/decoding',
     long_description=long_description,
