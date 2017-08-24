39a5048f03e146287a2df2807f23a0c4d60f29b8
mayeut <mayeut@users.noreply.github.com>
2017-08-25 00:22:07 +0200

    Better handling of temporary files in distutils

--- pybase64/distutils/ccompilercapabilities.py
+++ pybase64/distutils/ccompilercapabilities.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 import sys
+import tempfile
+from contextlib import contextmanager
 from distutils import log
 from distutils.errors import CompileError, LinkError
 
@@ -7,6 +10,43 @@ from distutils.errors import CompileError, LinkError
 __all__ = [
     'CCompilerCapabilities'
 ]
+
+
+@contextmanager
+def chdir(path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(cwd)
+
+
+@contextmanager
+def mkdtemp(suffix):
+    path = tempfile.mkdtemp(suffix)
+    try:
+        with chdir(path):
+            yield path
+    finally:
+        shutil.rmtree(path)
+
+
+@contextmanager
+def output(is_quiet):
+    if is_quiet:  # pragma: no branch
+        devnull = open(os.devnull, 'w')
+        oldstderr = os.dup(sys.stderr.fileno())
+        oldstdout = os.dup(sys.stdout.fileno())
+        os.dup2(devnull.fileno(), sys.stderr.fileno())
+        os.dup2(devnull.fileno(), sys.stdout.fileno())
+    try:
+        yield
+    finally:
+        if is_quiet:  # pragma: no branch
+            os.dup2(oldstderr, sys.stderr.fileno())
+            os.dup2(oldstdout, sys.stdout.fileno())
+            devnull.close()
 
 
 class CCompilerCapabilities:
@@ -24,50 +64,35 @@ class CCompilerCapabilities:
 
     def __has_simd_support(self, compiler, flags, include, content):
         quiet = True
-        # this can't be included at module scope because it tries to
-        # import math which might not be available at that point - maybe
-        # the necessary logic should just be inlined?
-        import tempfile
-        fd, fname = tempfile.mkstemp(".c", "simd", text=True)
-        f = os.fdopen(fd, "w")
-        try:
-            f.write("""#include <%s>\n""" % include)
-            f.write("""\
+
+        with mkdtemp("pybase64simdtest") as dname:
+            fname = os.path.join(dname, 'simd.c')
+            with open(fname, 'w') as f:
+                f.write("""#include <%s>\n""" % include)
+                f.write("""\
 int main (int argc, char **argv) {
     %s
 }
 """ % content)
-        finally:
-            f.close()
-
-        if quiet:  # pragma: no branch
-            devnull = open(os.devnull, 'w')
-            oldstderr = os.dup(sys.stderr.fileno())
-            oldstdout = os.dup(sys.stdout.fileno())
-            os.dup2(devnull.fileno(), sys.stderr.fileno())
-            os.dup2(devnull.fileno(), sys.stdout.fileno())
-        for flag in flags:
-            lflags = []
-            if not len(flag) == 0:
-                lflags = [flag]
-            try:
-                objects = compiler.compile([fname], extra_postargs=lflags)
-            except CompileError:
-                continue
-            try:
-                compiler.link_shared_lib(objects, "a.out")
-            except (LinkError, TypeError):  # pragma: no cover
-                continue  # pragma: no cover
-            if quiet:  # pragma: no branch
-                os.dup2(oldstderr, sys.stderr.fileno())
-                os.dup2(oldstdout, sys.stdout.fileno())
-                devnull.close()
-            return {'support': True, 'flags': lflags}
-        if quiet:  # pragma: no branch
-            os.dup2(oldstderr, sys.stderr.fileno())
-            os.dup2(oldstdout, sys.stdout.fileno())
-            devnull.close()
-        return {'support': False, 'flags': []}
+            with output(quiet):
+                for flag in flags:
+                    lflags = []
+                    if not len(flag) == 0:
+                        lflags = [flag]
+                    try:
+                        objects = compiler.compile(['simd.c'],
+                                                   output_dir=dname,
+                                                   extra_postargs=lflags)
+                    except CompileError:
+                        continue
+                    try:
+                        compiler.link_shared_lib(objects,
+                                                 'a.out',
+                                                 output_dir=dname)
+                    except (LinkError, TypeError):  # pragma: no cover
+                        continue  # pragma: no cover
+                    return {'support': True, 'flags': lflags}
+                return {'support': False, 'flags': []}
 
     def __get_capabilities(self, compiler):
         log.info("getting compiler simd support")
