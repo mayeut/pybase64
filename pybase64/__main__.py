d2da80f5b77de6168cdcc34cf4b709ab2ff38425
mayeut <mayeut@users.noreply.github.com>
2017-08-29 00:16:49 +0200

    Add argparse to main script
    This adds a help message, version and the benchmark tool.
    
    Updates #5

--- pybase64/__main__.py
+++ pybase64/__main__.py
@@ -1,6 +1,6 @@
+import argparse
 import base64
 import sys
-from codecs import open
 from timeit import default_timer as timer
 
 import pybase64
@@ -26,10 +26,10 @@ def bench_one(data, enc, dec, altchars=None, validate=False):
         encodedcontent = enc(data, altchars=altchars)
         iter -= 1
     time = timer() - time
-    print('{0:<32s} {1:9.3f} MB/s ({2:,d} bytes)'.format(
+    print('{0:<32s} {1:9.3f} MB/s ({2:,d} bytes -> {3:,d} bytes)'.format(
         enc.__module__ + '.' + enc.__name__ + ':',
         ((number * len(data)) / (1024.0 * 1024.0)) / time,
-        len(data)))
+        len(data), len(encodedcontent)))
     number = 0
     time = timer()
     while True:
@@ -47,14 +47,16 @@ def bench_one(data, enc, dec, altchars=None, validate=False):
                              validate=validate)
         iter -= 1
     time = timer() - time
-    print('{0:<32s} {1:9.3f} MB/s ({2:,d} bytes)'.format(
+    print('{0:<32s} {1:9.3f} MB/s ({3:,d} bytes -> {2:,d} bytes)'.format(
         dec.__module__ + '.' + dec.__name__ + ':',
         ((number * len(data)) / (1024.0 * 1024.0)) / time,
-        len(data)))
+        len(data), len(encodedcontent)))
     assert decodedcontent == data
 
 
-def bench(data):
+def benchmark(args):
+    print(__package__ + ' ' + pybase64.get_version())
+    data = args.input.read()
     for altchars in [None, b'-_']:
         for validate in [False, True]:
             print('bench: altchars={0:s}, validate={1:s}'.format(
@@ -72,11 +74,22 @@ def bench(data):
 
 
 def main(args=None):
-    print(pybase64.get_version())
-    with open(sys.argv[1], mode='rb') as f:
-        filecontent = f.read()
-    bench(filecontent)
-#    print(encodedcontent.decode('ascii'))
+    parser = argparse.ArgumentParser(
+        prog=__package__,
+        description=__package__ + ' command-line tool.')
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version=__package__ + ' ' + pybase64.get_version())
+    subparsers = parser.add_subparsers(help='tool help')
+    benchmark_parser = subparsers.add_parser('benchmark', help='-h for usage')
+    benchmark_parser.add_argument(
+        'input',
+        type=argparse.FileType('rb'),
+        help='input file used for the benchmark')
+    benchmark_parser.set_defaults(func=benchmark)
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":  # pragma: no branch
