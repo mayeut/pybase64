a2fbe6c1c797830ba0f85aa029370f84e0bd11f4
mayeut <mayeut@users.noreply.github.com>
2018-04-01 17:20:51 +0200

    Clean-up setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import glob
 import os
+import runpy
 import sys
 
 from setuptools import setup, find_packages, Extension
 from distutils.cmd import Command
 from distutils.command.build_ext import build_ext
+from distutils.errors import DistutilsSetupError
 from distutils.dep_util import newer_group
 from distutils import log
 from codecs import open
@@ -28,7 +30,8 @@ if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[:2] < (3, 3):
 here = path.abspath(path.dirname(__file__))
 
 # Get version
-exec(open(path.join(here, 'pybase64/_version.py')).read())
+version = runpy.run_path(
+    path.join(here, 'pybase64', '_version.py'))['__version__']
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -38,26 +41,26 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 with open(path.join(here, 'pybase64/_license.py'), 'wt') as f:
     f.write('_license = """')
     f.write('pybase64\n')
-    f.write('='*79 + '\n')
-    with open(path.join(here, 'LICENSE'), 'rt') as l:
-        f.write(l.read())
-    f.write('='*79 + '\n\n')
+    f.write('=' * 79 + '\n')
+    with open(path.join(here, 'LICENSE'), 'rt') as license:
+        f.write(license.read())
+    f.write('=' * 79 + '\n\n')
     f.write('libbase64\n')
-    f.write('='*79 + '\n')
-    with open(path.join(here, 'base64/LICENSE'), 'rt') as l:
-        f.write(l.read())
-    f.write('='*74)
+    f.write('=' * 79 + '\n')
+    with open(path.join(here, 'base64/LICENSE'), 'rt') as license:
+        f.write(license.read())
+    f.write('=' * 74)
     f.write('""" \\\n')
     f.write('    + "====="\n')
 
 pybase64_ext = Extension(
     "pybase64._pybase64",
     [
-       "pybase64/_pybase64.c",
-       "pybase64/_pybase64_get_simd_flags.c",
-       "base64/lib/lib.c",
-       "base64/lib/codec_choose.c",
-       "base64/lib/arch/generic/codec.c"
+        "pybase64/_pybase64.c",
+        "pybase64/_pybase64_get_simd_flags.c",
+        "base64/lib/lib.c",
+        "base64/lib/codec_choose.c",
+        "base64/lib/arch/generic/codec.c"
     ],
     include_dirs=["base64/include/", "base64/lib/"],
 )
@@ -74,38 +77,47 @@ pybase64_ext.sources_avx2 = ["base64/lib/arch/avx2/codec.c"]
 pybase64_ext.sources_neon32 = ["base64/lib/arch/neon32/codec.c"]
 pybase64_ext.sources_neon64 = ["base64/lib/arch/neon64/codec.c"]
 
+
 def pybase64_write_config(capabilities):
     log.info("creating 'base64/lib/config.h'")
     with open(path.join(here, 'base64/lib/config.h'), mode='wt') as f:
-        f.write('\n#define HAVE_SSSE3                 %i' % capabilities.has(CCompilerCapabilities.SIMD_SSSE3))
-        f.write('\n#define HAVE_SSE41                 %i' % capabilities.has(CCompilerCapabilities.SIMD_SSE41))
-        f.write('\n#define HAVE_SSE42                 %i' % capabilities.has(CCompilerCapabilities.SIMD_SSE42))
-        f.write('\n#define HAVE_AVX                   %i' % capabilities.has(CCompilerCapabilities.SIMD_AVX))
-        f.write('\n#define HAVE_AVX2                  %i' % capabilities.has(CCompilerCapabilities.SIMD_AVX2))
+        f.write('\n#define HAVE_SSSE3                 %i' %
+                capabilities.has(CCompilerCapabilities.SIMD_SSSE3))
+        f.write('\n#define HAVE_SSE41                 %i' %
+                capabilities.has(CCompilerCapabilities.SIMD_SSE41))
+        f.write('\n#define HAVE_SSE42                 %i' %
+                capabilities.has(CCompilerCapabilities.SIMD_SSE42))
+        f.write('\n#define HAVE_AVX                   %i' %
+                capabilities.has(CCompilerCapabilities.SIMD_AVX))
+        f.write('\n#define HAVE_AVX2                  %i' %
+                capabilities.has(CCompilerCapabilities.SIMD_AVX2))
         f.write('\n#define HAVE_NEON32                %i' % False)
         f.write('\n#define HAVE_NEON64                %i' % False)
         f.write('\n#define HAVE_FAST_UNALIGNED_ACCESS %i' % True)
         f.write('\n')
+
 
 class pybase64_build_ext(build_ext):
     def build_extension(self, ext):
         sources = ext.sources
         if sources is None or not isinstance(sources, (list, tuple)):
             raise DistutilsSetupError(
-                  "in 'ext_modules' option (extension '%s'), "
-                  "'sources' must be present and must be "
-                  "a list of source filenames" % ext.name)
+                "in 'ext_modules' option (extension '%s'), "
+                "'sources' must be present and must be "
+                "a list of source filenames" % ext.name)
         sources = list(sources)
 
         try:
             simd_sources = dict()
-            simd_sources[CCompilerCapabilities.SIMD_SSSE3]  = ext.sources_ssse3
-            simd_sources[CCompilerCapabilities.SIMD_SSE41]  = ext.sources_sse41
-            simd_sources[CCompilerCapabilities.SIMD_SSE42]  = ext.sources_sse42
-            simd_sources[CCompilerCapabilities.SIMD_AVX]    = ext.sources_avx
-            simd_sources[CCompilerCapabilities.SIMD_AVX2]   = ext.sources_avx2
-            simd_sources[CCompilerCapabilities.SIMD_NEON32] = ext.sources_neon32
-            simd_sources[CCompilerCapabilities.SIMD_NEON64] = ext.sources_neon64
+            simd_sources[CCompilerCapabilities.SIMD_SSSE3] = ext.sources_ssse3
+            simd_sources[CCompilerCapabilities.SIMD_SSE41] = ext.sources_sse41
+            simd_sources[CCompilerCapabilities.SIMD_SSE42] = ext.sources_sse42
+            simd_sources[CCompilerCapabilities.SIMD_AVX] = ext.sources_avx
+            simd_sources[CCompilerCapabilities.SIMD_AVX2] = ext.sources_avx2
+            simd_sources[CCompilerCapabilities.SIMD_NEON32] = \
+                ext.sources_neon32
+            simd_sources[CCompilerCapabilities.SIMD_NEON64] = \
+                ext.sources_neon64
 
             ext_path = self.get_ext_fullpath(ext.name)
             depends = sources + ext.depends
@@ -128,31 +140,33 @@ class pybase64_build_ext(build_ext):
 
             objects = list()
 
-            for simd_opt in (
-                CCompilerCapabilities.SIMD_SSSE3,
-                CCompilerCapabilities.SIMD_SSE41,
-                CCompilerCapabilities.SIMD_SSE42,
-                CCompilerCapabilities.SIMD_AVX,
-                CCompilerCapabilities.SIMD_AVX2,
-                CCompilerCapabilities.SIMD_NEON32,
-                CCompilerCapabilities.SIMD_NEON64):
-                if len(simd_sources[simd_opt]) > 0:
-                    if capabilities.has(simd_opt):
-                        objects = objects + self.compiler.compile(simd_sources[simd_opt],
-                                                                  output_dir=self.build_temp,
-                                                                  include_dirs=ext.include_dirs,
-                                                                  debug=self.debug,
-                                                                  extra_postargs=capabilities.flags(simd_opt),
-                                                                  depends=ext.depends)
-                    else:
-                        sources = sources + simd_sources[simd_opt]
+            for simd_opt in (CCompilerCapabilities.SIMD_SSSE3,
+                             CCompilerCapabilities.SIMD_SSE41,
+                             CCompilerCapabilities.SIMD_SSE42,
+                             CCompilerCapabilities.SIMD_AVX,
+                             CCompilerCapabilities.SIMD_AVX2,
+                             CCompilerCapabilities.SIMD_NEON32,
+                             CCompilerCapabilities.SIMD_NEON64):
+                if len(simd_sources[simd_opt]) == 0:
+                    continue
+                if capabilities.has(simd_opt):
+                    objects = objects + self.compiler.compile(
+                        simd_sources[simd_opt],
+                        output_dir=self.build_temp,
+                        include_dirs=ext.include_dirs,
+                        debug=self.debug,
+                        extra_postargs=capabilities.flags(simd_opt),
+                        depends=ext.depends)
+                else:
+                    sources = sources + simd_sources[simd_opt]
 
-            objects = objects + self.compiler.compile(sources,
-                                                      output_dir=self.build_temp,
-                                                      include_dirs=ext.include_dirs,
-                                                      debug=self.debug,
-                                                      extra_postargs=[],
-                                                      depends=ext.depends)
+            objects = objects + self.compiler.compile(
+                sources,
+                output_dir=self.build_temp,
+                include_dirs=ext.include_dirs,
+                debug=self.debug,
+                extra_postargs=[],
+                depends=ext.depends)
 
             # Detect target language, if not provided
             language = ext.language or self.compiler.detect_language(sources)
@@ -166,12 +180,13 @@ class pybase64_build_ext(build_ext):
                 debug=self.debug,
                 build_temp=self.build_temp,
                 target_lang=language)
-        except:
+        except:  # noqa
             if ext.optional:
                 log.warn("warning: Extension %s could not be built" % ext.name)
             else:
                 log.error("error: Extension %s could not be built" % ext.name)
                 raise
+
 
 # Let's define a class to clean in-place built extensions
 class CleanExtensionCommand(Command):
@@ -203,33 +218,33 @@ exts.append(
 )
 
 setup(
-    name = 'pybase64',
-    cmdclass = {
+    name='pybase64',
+    cmdclass={
         "build_ext": pybase64_build_ext,
         'clean_ext': CleanExtensionCommand,
     },
-    ext_modules = exts,
+    ext_modules=exts,
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version = __version__,
+    version=version,
 
-    description = 'Fast Base64 encoding/decoding',
-    long_description = long_description,
+    description='Fast Base64 encoding/decoding',
+    long_description=long_description,
 
     # The project's main homepage.
-    url = 'https://github.com/mayeut/pybase64',
+    url='https://github.com/mayeut/pybase64',
 
     # Author details
-    author = 'Matthieu Darbois',
-    #author_email = 'mayeut@users.noreply.github.com',
+    author='Matthieu Darbois',
+    # author_email = 'mayeut@users.noreply.github.com',
 
     # Choose your license
-    license = 'BSD-2-Clause',
+    license='BSD-2-Clause',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers = [
+    classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
@@ -260,12 +275,12 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
 
     # What does your project relate to?
-    keywords = 'base64',
+    keywords='base64',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages = find_packages(),
-    install_requires = ['six>=1.10.0'],
+    packages=find_packages(),
+    install_requires=['six>=1.10.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -278,7 +293,7 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'pybase64=pybase64.__main__:main',
         ],
