c85c0b9255b41f4c88b78d28cddfc8dd642410b4
mayeut <mayeut@users.noreply.github.com>
2018-04-01 18:49:29 +0200

    Use travis-ci builtin deployment

---
+++ Brewfile
@@ -0,0 +1,2 @@
+brew 'openssl'
+brew 'python'
--- travis/build-source-dist.sh
+++ travis/build-source-dist.sh
@@ -10,15 +10,10 @@ pushd from_source
 tar -xf ../dist/pybase64*.tar.gz
 pushd pybase64*
 # make extension mandatory
-touch .cibuildwheel
+export CIBUILDWHEEL=1
 # build extension
 python setup.py build_ext -i -f
 # test
 pytest
 popd
 popd
-
-# all is ok, move sdist in todelpoy folder
-mkdir todeploy
-mv dist/pybase64*.tar.gz todeploy/
-
