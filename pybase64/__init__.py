b89329d9d818cd8609c294cff1ccbf4c7c759aba
Matthieu Darbois <mayeut@users.noreply.github.com>
2020-12-30 21:08:47 +0100

    Use pre-commit, blacken the code (#224)

--- pybase64/__init__.py
+++ pybase64/__init__.py
@@ -1,6 +1,5 @@
 from ._version import __version__
 
-
 try:
     __PYBASE64_SETUP__
 except NameError:
@@ -9,15 +8,28 @@ except NameError:
 # Only go on if not in setup.py
 if not __PYBASE64_SETUP__:
     from ._license import _license
+
     try:
-        from ._pybase64 import (_get_simd_path, b64decode,  # noqa: F401
-                                b64decode_as_bytearray, b64encode,
-                                b64encode_as_string, encodebytes)
+        from ._pybase64 import (  # noqa: F401
+            _get_simd_path,
+            b64decode,
+            b64decode_as_bytearray,
+            b64encode,
+            b64encode_as_string,
+            encodebytes,
+        )
+
         _has_extension = True
     except ImportError:
-        from ._fallback import (_get_simd_path, b64decode,  # noqa: F401
-                                b64decode_as_bytearray, b64encode,
-                                b64encode_as_string, encodebytes)
+        from ._fallback import (  # noqa: F401
+            _get_simd_path,
+            b64decode,
+            b64decode_as_bytearray,
+            b64encode,
+            b64encode_as_string,
+            encodebytes,
+        )
+
         _has_extension = False
 
     def get_license_text():
@@ -37,21 +49,21 @@ if not __PYBASE64_SETUP__:
             simd_name = None
             simd_flag = _get_simd_path()
             if simd_flag == 0:
-                simd_name = 'No SIMD'
+                simd_name = "No SIMD"
             elif simd_flag == 4:
-                simd_name = 'SSSE3'
+                simd_name = "SSSE3"
             elif simd_flag == 8:
-                simd_name = 'SSE41'
+                simd_name = "SSE41"
             elif simd_flag == 16:
-                simd_name = 'SSE42'
+                simd_name = "SSE42"
             elif simd_flag == 32:
-                simd_name = 'AVX'
+                simd_name = "AVX"
             elif simd_flag == 64:
-                simd_name = 'AVX2'
+                simd_name = "AVX2"
             else:  # pragma: no branch
-                simd_name = 'Unknown'  # pragma: no cover
-            return __version__ + ' (C extension active - ' + simd_name + ')'
-        return __version__ + ' (C extension inactive)'
+                simd_name = "Unknown"  # pragma: no cover
+            return __version__ + " (C extension active - " + simd_name + ")"
+        return __version__ + " (C extension inactive)"
 
     def standard_b64encode(s):
         """Encode bytes using the standard Base64 alphabet.
@@ -86,7 +98,7 @@ if not __PYBASE64_SETUP__:
 
         The alphabet uses '-' instead of '+' and '_' instead of '/'.
         """
-        return b64encode(s, b'-_')
+        return b64encode(s, b"-_")
 
     def urlsafe_b64decode(s):
         """Decode bytes using the URL- and filesystem-safe Base64 alphabet.
@@ -103,4 +115,4 @@ if not __PYBASE64_SETUP__:
 
         The alphabet uses '-' instead of '+' and '_' instead of '/'.
         """
-        return b64decode(s, b'-_')
+        return b64decode(s, b"-_")
--- pybase64/distutils/__init__.py
+++
@@ -1,8 +0,0 @@
-# This relies on each of the submodules having an __all__ variable.
-
-from .ccompilercapabilities import *
-
-
-__all__ = (
-    ccompilercapabilities.__all__
-)
