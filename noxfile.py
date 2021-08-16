5a8e8f9dea266d0cf69f3a3bf3133ed32ba0becf
mayeut <mayeut@users.noreply.github.com>
2021-08-16 23:46:58 +0200

    chore: run tests using nox

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import platform
+import sys
+
 import nox
 
 
@@ -8,3 +11,17 @@ def lint(session: nox.Session) -> None:
     """
     session.install("pre-commit")
     session.run("pre-commit", "run", "-a")
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
+def test(session: nox.Session) -> None:
+    """
+    Run tests.
+    """
+    # make extension mandatory by exporting CIBUILDWHEEL=1
+    env = {"CIBUILDWHEEL": "1"}
+    if sys.platform.startswith("darwin"):
+        # we don't support universal builds
+        env["ARCHFLAGS"] = f"-arch {platform.machine()}"
+    session.install("--use-feature=in-tree-build", ".[test]", env=env)
+    session.run("pytest", "--pyargs", "pybase64")
