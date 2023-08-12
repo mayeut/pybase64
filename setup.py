50934bcb2c819945036d5bb9e52eb284c8a5bb8a
mayeut <mayeut@users.noreply.github.com>
2023-08-12 17:14:45 +0200

    chore: add trove classifier for Python 3.12

--- setup.py
+++ setup.py
@@ -234,6 +234,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     # Supported python versions
     python_requires=">=3.6",
