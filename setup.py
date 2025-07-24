d4ac7f3e0e6620e56459c5740e6c38474d64dd6d
Matthieu Darbois <mayeut@users.noreply.github.com>
2025-07-24 21:17:15 +0200

    feat: add python 3.14 support (#884)

--- setup.py
+++ setup.py
@@ -253,6 +253,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     # Supported python versions
     python_requires=">=3.8",
